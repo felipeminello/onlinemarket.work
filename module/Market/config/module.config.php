b0023923808ed16c1c50f9af3de92501fa182296
Felipe Minello <felipeminello@gmail.com>
2015-05-16 14:15:35 -0300

    M7EX1 - Rotas

--- module/Market/config/module.config.php
+++ module/Market/config/module.config.php
@@ -2,9 +2,8 @@
 return array(
     'controllers' => array(
         'invokables' => array(
-//            'Market\Controller\Index' => 'Market\Controller\IndexController',
             'market-index-controller' => 'Market\Controller\IndexController',
-            'market-view-controller' => 'Market\Controller\ViewController',
+            'market-view-controller' => 'Market\Controller\ViewController'
         ),
         'factories' => array(
             'market-post-controller' => 'Market\Factory\PostControllerFactory'
@@ -15,6 +14,76 @@ return array(
     ),
     'router' => array(
         'routes' => array(
+            'home' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/',
+                    'defaults' => array(
+                        'controller' => 'market-index-controller',
+                        'action' => 'index'
+                    )
+                )
+            ),
+            'market' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/market',
+                    'defaults' => array(
+                        'controller' => 'market-index-controller',
+                        'action' => 'index'
+                    )
+                ),
+                
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'view' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/view',
+                            'defaults' => array(
+                                'controller' => 'market-view-controller',
+                                'action' => 'index'
+                            )
+                        ),
+                        'may_terminate' => true,
+                        'child_routes' => array(
+                            'main' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/main[/:category]',
+                                    'defaults' => array(
+                                        'action' => 'index'
+                                    )
+                                )
+                            ),
+                            'item' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/item[/:itemId]',
+                                    'defaults' => array(
+                                        'action' => 'item'
+                                    ),
+                                    'constraints' => array(
+                                        'itemId' => '[0-9]*'
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                    'post' => array(
+                        'type' => 'Segment',
+                        'options' => array(
+                            'route' => '/post',
+                            'defaults' => array(
+                                'controller' => 'market-post-controller',
+                                'action' => 'index'
+                            )
+                        )
+                    )
+                )
+            ),
+            
+/*            
             'market' => array(
                 'type'    => 'Literal',
                 'options' => array(
@@ -48,11 +117,12 @@ return array(
                     ),
                 ),
             ),
-        ),
+*/            
+        )
     ),
     'view_manager' => array(
         'template_path_stack' => array(
-            'Market' => __DIR__ . '/../view',
-        ),
-    ),
+            'Market' => __DIR__ . '/../view'
+        )
+    )
 );
--- module/Market/src/Market/Controller/ViewController.php
+++ module/Market/src/Market/Controller/ViewController.php
@@ -13,14 +13,35 @@ class ViewController extends AbstractActionController
 {
     public function indexAction()
     {
-        $category = $this->params()->fromQuery('category');
+        /**
+         * Pega a categoria de uma QueryString
+         * EX: /onlinemarket.work/public/market/market-view-controller/index?category=teste
+         */
+//        $category = $this->params()->fromQuery('category');
+        
+        
+        /**
+         * Pega a categoria de uma Rota
+         * EX: /onlinemarket.work/public/market/view/main/teste
+         */
+        $category = $this->params()->fromRoute('category');
         
         return new ViewModel(['category' => $category]);
     }
     
     public function itemAction()
     {
-        $itemId = $this->params()->fromQuery('itemId');
+        /**
+         * Pega a categoria de uma QueryString
+         * EX: /onlinemarket.work/public/market/market-view-controller/item?itemId=123
+         */
+//        $itemId = $this->params()->fromQuery('itemId');
+        
+        /**
+         * Pega a categoria de uma QueryString
+         * EX: /onlinemarket.work/public/market/market-view-controller/item?itemId=123
+         */
+        $itemId = $this->params()->fromRoute('itemId');
         
         if (empty($itemId)) {
 //            $this->redirect()->toRoute('market');
